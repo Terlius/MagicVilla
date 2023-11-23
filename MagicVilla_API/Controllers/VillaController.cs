b980836569e2877290d11b71fc589f86958cf8cf
Miguel Mateo Grajales <matius404@hotmail.com>
2023-11-22 21:00:58 -0500

    Models, DTO and Controller

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,38 @@
+﻿using MagicVilla_API.Data;
+using MagicVilla_API.Models;
+using MagicVilla_API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.VillaList);
+
+        }
+
+        [HttpGet("id:int")]
+
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.VillaList.FirstOrDefault(v => v.Id == id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+           return Ok(villa);
+            
+        }
+    
+    }
+}
---
+++ MagicVilla_API/Data/VillaStore.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_API.Models.DTO;
+
+namespace MagicVilla_API.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> VillaList = new List<VillaDTO>
+        {
+            new VillaDTO{Id= 1, Name = "Villa 1"},
+            new VillaDTO{Id= 2, Name = "Villa 2"}
+        };
+
+    }
+}
